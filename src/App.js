d31f2491e88410aa5513f5486c8b5ce2120bdb3d
Xoneris <kely42@gmx.ch>
2024-04-22 04:19:30 +0200

    Added Vercel Analytics & Speed-insights

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { BrowserRouter} from 'react-router-dom';
 import Homepage from './homepage/Homepage';
 
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 function App() {
 
   return (
     <>
+      <Analytics/>
+      <SpeedInsights/>
+      
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Homepage />
       </BrowserRouter>
