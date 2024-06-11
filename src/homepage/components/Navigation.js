b8a926e45cf1f62d9d1004b2aa4449316f211cd4
Xoneris <kely42@gmx.ch>
2024-06-11 02:34:51 +0200

    Added  Platforms to the Navigation!

--- src/homepage/components/Navigation.js
+++ src/homepage/components/Navigation.js
@@ -28,8 +28,20 @@ function Navigation() {
                 </div>
                 <NavLink to="/Demo"><li>Demos</li></NavLink>
                 <NavLink to="/Released"><li>Released</li></NavLink>
+                <div className="dropdown-item">
+                    <li>Platforms</li>
+                    <div className="dropdown-content">
+                        <ul>
+                            <NavLink to="/Steam"><li>Steam</li></NavLink >
+                            <NavLink to="/Epic"><li>Epic Games</li></NavLink>
+                            <NavLink to="/GOG"><li>GOG</li></NavLink>
+                            <NavLink to="/Playstation"><li>Playstation</li></NavLink>
+                            <NavLink to="/Xbox"><li>Xbox</li></NavLink>
+                            <NavLink to="/switch"><li>Nintendo Switch</li></NavLink>
+                        </ul>
+                    </div>
+                </div>
                 <NavLink to="/AllGames"><li>All Games</li></NavLink>
-                {/* <NavLink to="/Search"><li>Search</li></NavLink> */}
                 <div className="nav-search">
                     <input type="text" onClick={() => {searchBox.setShowSearch(!searchBox.showSearch)}}  />
                 </div>
@@ -59,13 +71,22 @@ function Navigation() {
                         <NavLink to="/TBD" onClick={() => setNavOpen(!navOpen)}><li>TBD</li></NavLink>
                         <NavLink to="/EarlyAccess" onClick={() => setNavOpen(!navOpen)}><li>Early Access</li></NavLink>
                         <NavLink to="/Kickstarter" onClick={() => setNavOpen(!navOpen)}><li>Upcoming Kickstarter</li></NavLink>
-
                     </ul>
                 </li><hr/>
                 <NavLink to="/Demo" onClick={() => setNavOpen(!navOpen)}><li>Demos</li></NavLink><hr/>
                 <NavLink to="/Released" onClick={() => setNavOpen(!navOpen)}><li>Released</li></NavLink><hr/>
-                <NavLink to="/AllGames" onClick={() => setNavOpen(!navOpen)}><li>All Games</li></NavLink><hr/>
-                {/* <NavLink to="/Search" onClick={() => setNavOpen(!navOpen)}><li>Search</li></NavLink><hr/> */}
+                <li>Platforms</li>
+                <li>
+                    <ul>
+                        <NavLink to="/Steam" onClick={() => setNavOpen(!navOpen)}><li>Steam</li></NavLink >
+                        <NavLink to="/Epic" onClick={() => setNavOpen(!navOpen)}><li>Epic Games</li></NavLink>
+                        <NavLink to="/GOG" onClick={() => setNavOpen(!navOpen)}><li>GOG</li></NavLink>
+                        <NavLink to="/Playstation" onClick={() => setNavOpen(!navOpen)}><li>Playstation</li></NavLink>
+                        <NavLink to="/Xbox" onClick={() => setNavOpen(!navOpen)}><li>Xbox</li></NavLink>
+                        <NavLink to="/switch" onClick={() => setNavOpen(!navOpen)}><li>Nintendo Switch</li></NavLink>
+                    </ul>
+                </li><hr/>
+                <NavLink to="/AllGames" onClick={() => setNavOpen(!navOpen)}><li>All Games</li></NavLink>
             </ul>
             </div>
         </nav>
